f9cb26e877a2779687c0aff49fd85e69e43635bf
Edua Kaszas <edua.kaszas@gmail.com>
2021-10-15 23:23:11 +0200

    feat(gym-app): configured libraries

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './components/Entry/Entry';
+import App from './components/Entry';
 
 test('renders learn react link', () => {
   render(<App />);
